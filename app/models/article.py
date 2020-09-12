79f6f4ade8fe8276e3884e08ef30658949395553
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-09-12 19:45:50 +0300

    Added save_article method to class Article

--- app/models/article.py
+++ app/models/article.py
@@ -3,9 +3,14 @@ class Article:
     all_articles[]
     def __init__(self,source,title,author,description,link,image_url):
         ''' __init__ method to create Article objects'''
-            self.source = source
-            self.title = title
-            self.author = author
-            self.description = description
-            self.link = link
-            self.image_url = image_url
+        self.source = source
+        self.title = title
+        self.author = author
+        self.description = description
+        self.link = link
+        self.image_url = image_url
+
+@classmethod
+def save_article(cls,self):
+    '''Article class method that saves an article into the all_articles list'''
+    cls.all_articles.append(self)
