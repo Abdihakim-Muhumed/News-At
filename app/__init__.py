d03f951de8f98b04a915c2c18e6b1d6443f0af12
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-09-12 17:30:21 +0300

    Installed flask_bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from .config import DevConfig
-# Initializing app 
+from flask_bootstrap import Bootstrap
 
+
+# Initializing app 
 app = Flask(__name__,instance_relative_config=True)
 
-
+# setting configs
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
+#initializing bootstrap
+bootstrap = Bootstrap(app)
 from app import views 
