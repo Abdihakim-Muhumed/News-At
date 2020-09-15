cf468f7f43f7c80f603fac9dcd19dbb1151f4f73
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-09-15 08:53:24 +0300

    Added function to request headlines from the api in request.py

--- app/request.py
+++ app/request.py
@@ -9,6 +9,7 @@ api_key = app.config["MY_KEY"]
 #getting the base url
 source_base_url = app.config['SOURCES_URL']
 article_base_url = app.config["ARTICLES_URL"]
+headlines_base_url = app.config["HEADLINES_URL"]
 
 def get_sources():
     '''function to get data from the api url
@@ -68,6 +69,50 @@ def get_articles(id):
             articles_results_lists = get_articles_response["articles"]
             articles_results = process_articles_results(articles_results_lists)
         
+    return articles_results
+
+
+def get_headlines():
+    '''function to get top headlines 
+        returns:
+            headlines_resultd:a list of top articles
+        '''
+    get_headlines_url = headlines_base_url.format(api_key)
+    with urllib.request.urlopen(get_headlines_url) as url:
+        get_headlines_data = url.read()
+        get_headlines_response = json.loads(get_headlines_data)
+
+
+        headlines_results = None
+
+        if get_headlines_response["articles"]:
+            headlines_results_lists = get_headlines_response["articles"]
+            headlines_results = process_headlines_results(headlines_results_lists)
+        
+    return headlines_results
+
+def process_headlines_results(headlines_list):
+    ''' Function to process the results from get_headlines function
+        args:
+            headlines_list: alist of dictionaries from get_headlines function
+        returns:
+            articles_results: a list of Article objects'''        
+    
+    articles_results = []
+    counter = 0
+    for article in headlines_list:
+        title = article.get('title')
+        author = article.get('author')
+        description = article.get('description')
+        link = article.get('url')
+        image_url = article.get('urlToImage')
+        published_time = article.get('publishedAt')
+        article_object = Article(title,author,description,link,image_url,published_time)
+        articles_results.append(article_object)
+        counter +=1
+        if counter == 5:
+            break 
+
     return articles_results
 
 def process_articles_results(articles_list):
